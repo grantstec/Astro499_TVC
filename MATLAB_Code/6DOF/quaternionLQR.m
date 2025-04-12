1408349266de3ff1bb8e7e86456cde750202f73c
LucanKeyserUSAFA <C27LUCAN.KEYSER@AFACADEMY.AF.EDU>
2025-04-11 20:40:49 -0600

    Combination of Gravity and LQR

--- MATLAB_Code/6DOF/quaternionLQR.m
+++ MATLAB_Code/6DOF/quaternionLQR.m
@@ -10,7 +10,8 @@ Q = diag([100,100,100,1,1,1]); %[qe1, qe2, qe3, w1, w2, w3]
 R = diag([1,1,1]);%equal control cost
 K = lqr(A,B, Q, R); %find K
 
-init = [.2, .3, .5]; %orientation
-w0 = [0, 0.5, 0.5];
+init = [0, 0.5, 0.5]; %orientation (of flight comp. guess)
+dt = 0.01;
+w0 = [0, 0, 0]; %body angular rates
 %lqr referenced: 
 %https://www.sciencedirect.com/science/article/pii/S1367578812000387?ref=pdf_download&fr=RR-2&rr=92dbcffa994d1f34
